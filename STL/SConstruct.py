1048d367db24a0ef675e859d6040c7487d4e1a2e
Manuel Giffels <giffels@gmail.com>
2015-06-10 13:57:10 +0200

    Use modified SConstruct.py

--- STL/SConstruct.py
+++ STL/SConstruct.py
@@ -1,3 +1,7 @@
-Program(target="MuonExercise01", source=["MuonExercise01.cpp"], CCFLAGS="-std=c++11 -Wall")
+import glob
+import os
+
+for source_file in glob.glob('*.cpp'):
+    Program(target=os.path.splitext(source_file)[0], source=[source_file], CCFLAGS="-std=c++11 -Wall")
 
 
